18338b56a67f7d33906e0b12f196de959ff9a4b2
Olivier Spinelli <olivier.spinelli@invenietis.com>
2015-03-27 15:41:12 +0100

    my linq works.

--- FirstSolution/Tests/ITI.Misc.Tests/MyLinq/MyLinq.cs
+++ FirstSolution/Tests/ITI.Misc.Tests/MyLinq/MyLinq.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,35 @@ using System.Threading.Tasks;
 
 namespace MyLinq
 {
-    class MyLinq
+    public static class EnumerableExtension
     {
+        public static IEnumerable<T> Where<T>( this IEnumerable<T> objects, Func<T, bool> filter )
+        {
+            foreach( T o in objects )
+            {
+                if( filter( o ) ) yield return o;
+            }
+        }
+
+        public static int Sum( this IEnumerable<int> integers )
+        {
+            int total = 0;
+            foreach( int i in integers ) total += i;
+            return total;
+        }
+
+        public static IEnumerable<T> OfType<T>( this IEnumerable objects )
+        {
+            foreach( object o in objects )
+            {
+                if( o is T ) yield return (T)o;
+            }
+        }
+
+        public static IEnumerable<TOutput> Select<TInput, TOutput>( this IEnumerable<TInput> a, Func<TInput, TOutput> f )
+        {
+            foreach( TInput i in a )
+                yield return f( i );
+        }
     }
 }
--- FirstSolution/Tests/ITI.Misc.Tests/MyLinq/MyLinqTests.cs
+++ FirstSolution/Tests/ITI.Misc.Tests/MyLinq/MyLinqTests.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using MyLinq;
 
 namespace ITI.Misc.Tests
 {
@@ -16,14 +18,62 @@ namespace ITI.Misc.Tests
             _objects = new object[]
             {
                 21, 578, 87, 78, "A", "B", "C", "A", 
-                "D", "U" , new ListSimpleBottle(), typeof(string), new Exception(),
+                "D", "U" , new ListSimpleBottle(), 
+                new ListSimpleBottle(), new ListSimpleBottle(), new ListSimpleBottle(), new ListSimpleBottle(), 
+                typeof(string), new Exception(),
                 new OutOfMemoryException(), this
             };
         }
 
         public void how_a_list_can_be_filtered()
         {
+            IEnumerable<object> a = _objects;
+            {
+                IEnumerable<string> s = ToObjectString( a );
+                IEnumerable<Type> types = ToObjectTypes( a );
+            }
+            {
+                IEnumerable<string> s = EnumerableExtension.Select( _objects, o => o.ToString() );
+                IEnumerable<Type> types = EnumerableExtension.Select( _objects, o => o.GetType() );
 
+                IEnumerable<string> s2 = _objects.Select( o => o.ToString() );
+                IEnumerable<Type> types2 = _objects.Select( o => o.GetType() );
+            }
+            // Filtering for types
+            IEnumerable<int> integers = EnumerableExtension.OfType<int>( _objects );
+            int sum = EnumerableExtension.Sum( integers );
+
+            IEnumerable<int> bigIntegers = EnumerableExtension.Where( integers, i => i > 2 );
+        }
+
+        public void filtering_is_easy()
+        {
+            Console.WriteLine( "Sum = {0}", _objects.OfType<int>().Sum() );
+            foreach( var i in _objects.OfType<int>() )
+            {
+                //...
+            }
+            int sumOfEven = _objects
+                                .OfType<int>()
+                                .Where( i => i % 2 == 0 )
+                                .Sum();
+            int sumOfOdd = _objects
+                                .OfType<int>()
+                                .Where( i => i % 2 == 1 )
+                                .Sum();
+            Console.WriteLine( "even = {0}, odd = {1}", sumOfEven, sumOfOdd );
+        }
+
+        private IEnumerable<string> ToObjectString( IEnumerable<object> a )
+        {
+            foreach( object o in a )
+                yield return o.ToString();
+        }
+
+        private IEnumerable<Type> ToObjectTypes( IEnumerable<object> a )
+        {
+            foreach( object o in a )
+                yield return o.GetType();
         }
 
     }
