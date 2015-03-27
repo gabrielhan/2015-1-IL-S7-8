a24e856f11febe8be0d4c98bf740ac5232d4b591
Olivier Spinelli <olivier.spinelli@invenietis.com>
2015-03-27 14:16:09 +0100

    mylinq

---
+++ FirstSolution/Tests/ITI.Misc.Tests/MyLinq/MyLinqTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ITI.Misc.Tests
+{
+    [TestFixture]
+    public class MyLinqTests
+    {
+        object[] _objects;
+        
+        public MyLinqTests()
+        {
+            _objects = new object[]
+            {
+                21, 578, 87, 78, "A", "B", "C", "A", 
+                "D", "U" , new ListSimpleBottle(), typeof(string), new Exception(),
+                new OutOfMemoryException(), this
+            };
+        }
+
+        public void how_a_list_can_be_filtered()
+        {
+
+        }
+
+    }
+}
