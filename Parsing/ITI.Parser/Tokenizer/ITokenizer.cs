1983a5b4227000b737be1ad673a25e243b8f5c3e
Olivier Spinelli <olivier.spinelli@invenietis.com>
2015-05-18 14:51:20 +0200

    Tokenizer.

---
+++ Parsing/ITI.Parser/Tokenizer/ITokenizer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITI.Parser
+{
+    public interface ITokenizer
+    {
+        TokenType CurrentToken { get; }
+
+        TokenType GetNextToken();
+
+        /// <summary>
+        /// Checks that the <see cref="CurrentToken"/> is equal
+        /// to <paramref name="t"/> and forwards the head on success.
+        /// </summary>
+        /// <param name="t">Type of the expected token.</param>
+        /// <returns>True if token is of the given type.</returns>
+        bool Match( TokenType t );
+
+        bool MatchInteger( int expected );
+
+        bool MatchInteger( out int value );
+
+        bool MatchDouble( out double value );
+
+    }
+}
