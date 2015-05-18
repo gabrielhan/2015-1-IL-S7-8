36773e02ba38adcced1f8e3748c33636e8f6c49a
Olivier Spinelli <olivier.spinelli@invenietis.com>
2015-05-18 15:09:16 +0200

    Unit Tests...

---
+++ Parsing/ITI.Parser.Tests/TokenizerTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ITI.Parser.Tests
+{
+    [TestFixture]
+    public class TokenizerTests
+    {
+        [TestCase( "3" )]
+        [TestCase( "3 8" )]
+        [TestCase( "3 8 12 8787 3712" )]
+        public void list_of_numbers( string toParse )
+        {
+            var t = new StringTokenizer( toParse );
+            List<double> values = new List<double>();
+            t.GetNextToken();
+            while( t.CurrentToken != TokenType.EndOfInput )
+            {
+                Assert.That( t.CurrentToken == TokenType.Number );
+                double v;
+                Assert.That( t.MatchDouble( out v ) );
+                values.Add( v );
+            }
+            CollectionAssert.AreEqual( toParse.Split( ' ' ).Select( s => Double.Parse( s ) ).ToList(), values );
+        }
+
+
+    }
+}
