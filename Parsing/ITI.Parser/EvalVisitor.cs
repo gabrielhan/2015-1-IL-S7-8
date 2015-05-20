e7ec70ba628c3ae45be683921a524180d1404548
gabrielhan <hannaby@intechinfo.fr>
2015-05-20 10:35:53 +0200

    question working

--- Parsing/ITI.Parser/EvalVisitor.cs
+++ Parsing/ITI.Parser/EvalVisitor.cs
@@ -46,7 +46,46 @@ namespace gaby.Parser
 {
     public class EvalVisitor : NodeVisitor
     {
+        double _currentValue;
 
-        public double Result { get; set; }
+        public double Result { get { return _currentValue; } }
+
+        public override void Visit(BinaryNode n)
+        {
+            VisitNode(n.Left);
+            var left = _currentValue;
+            VisitNode(n.Right);
+            var right = _currentValue;
+            switch (n.OperatorType)
+            {
+                case TokenType.Mult: _currentValue = left * right; break;
+                case TokenType.Div: _currentValue = left / right; break;
+                case TokenType.Plus: _currentValue = left + right; break;
+                case TokenType.Minus: _currentValue = left - right; break;
+            }
+        }
+
+        public override void Visit(UnaryNode n)
+        {
+            VisitNode(n.Right);
+            _currentValue = -_currentValue;
+        }
+
+        public override void Visit(ConstantNode n)
+        {
+            _currentValue = n.Value;
+        }
+
+        public override void Visit(IfNode n)
+        {
+            VisitNode(n.Condition);
+            if(_currentValue >= 0)
+            {
+                VisitNode(n.WhenTrue);
+            }else
+            {
+                VisitNode(n.WhenFalse);
+            }
+        }
     }
 }
